48cd8f24a794ee2bd971ee5af55d72740f11f1aa
ralphandrade <ralphandrade06@gmail.com>
2024-05-23 21:45:41 -0300

    altera lista

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 # lista
-nomes = ['Alex', 'Fabio', 'Morgana', 'Tiago', 'Emanuelle','Joao', 'José', 'Maria', 'Francisco', 'Guilherme', 'Gustavo', 'Beatriz','Sofia', 'Yasmin','Rogério']
+nomes = ['Alex', 'Fabio', 'Morgana', 'Tiago', 'Emanuelle']
 
-# usuario informa o nome que deseja pesquisar
-nome = input('Digite o nome a ser pesquisado: ').capitalize()
+# usuario informa o indice que deseja alterar
+indice = int(input('Informe o indice que deseja alterar: '))
+indice -= 1
 
-#retorna o indice do nome pesquisado
-# indice = nomes.index(nome)
+# usuario informa o novo nome
+nomes[indice] = input("Informe o nove nome: ")
 
-# verifique se o nome está na lista ou não
-try:
-    indice = nomes.index(nome)
-    print(f'Nomwe encontrado:{nome} no indice{indice}.')
-except:
-    print(f'{nome} não encotrado na lista. ')
+# exibe a lista
+for nome in nomes:
+    print(nome)
 
    
 
